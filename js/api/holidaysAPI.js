cc43740b585e2ab3be957ef20d9a05f4ec6e8d96
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-07-29 01:35:57 +0800

    Add public holidays for 17 countries in 2024
    - Added holiday data for Brunei, Anguilla, Antigua and Barbuda, Aruba, Curaçao, Dominica, French Guiana, Malaysia, Guadeloupe, Philippines, Saint Kitts and Nevis, Saint Lucia, Saint Vincent and the Grenadines, Trinidad and Tobago, Turks and Caicos Islands, and U.S. Virgin Islands for the year 2024.
    - Updated module structure to support additional years for easy future expansion.

---
+++ js/api/holidaysAPI.js
@@ -0,0 +1,176 @@
+// HolidayAPI.js
+
+const holidays = {
+    2024: {
+        Brunei: [
+            "New Year's Day - January 1",
+            "Chinese New Year - February 10",
+            "Hari Raya Aidilfitri - April 10",
+            "Hari Raya Aidiladha - June 17",
+            "Awal Muharram - July 7",
+            "Maulidur Rasul - September 15",
+            "National Day - February 23",
+            "Sultan's Birthday - July 15"
+        ],
+        Anguilla: [
+            "New Year's Day - January 1",
+            "Heritage Day - May 30",
+            "Queen's Birthday - June 10",
+            "Emancipation Day - August 5",
+            "Anguilla Day - May 30",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'Antigua and Barbuda': [
+            "New Year's Day - January 1",
+            "Antigua and Barbuda Independence Day - November 1",
+            "Carnival Monday - August 5",
+            "Carnival Tuesday - August 6",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        Aruba: [
+            "New Year's Day - January 1",
+            "Carnival - February 12",
+            "Good Friday - March 29",
+            "King's Day - April 27",
+            "Labor Day - May 1",
+            "Ascension Day - May 30",
+            "Whit Monday - June 10",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        Curaçao: [
+            "New Year's Day - January 1",
+            "Carnival - February 12",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "King's Day - April 27",
+            "Labor Day - May 1",
+            "Ascension Day - May 30",
+            "Whit Monday - June 10",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        Dominica: [
+            "New Year's Day - January 1",
+            "Carnival - February 12",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labor Day - May 1",
+            "Independence Day - November 3",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'French Guiana': [
+            "New Year's Day - January 1",
+            "Carnival - February 12",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labour Day - May 1",
+            "Bastille Day - July 14",
+            "Assumption Day - August 15",
+            "All Saints' Day - November 1",
+            "Christmas Day - December 25"
+        ],
+        Malaysia: [
+            "New Year's Day - January 1",
+            "Chinese New Year - February 10",
+            "Hari Raya Aidilfitri - April 10",
+            "Hari Raya Aidiladha - June 17",
+            "Awal Muharram - July 7",
+            "Maulidur Rasul - September 15",
+            "Deepavali - October 31",
+            "Christmas Day - December 25",
+            "Labour Day - May 1"
+        ],
+        Guadeloupe: [
+            "New Year's Day - January 1",
+            "Carnival - February 12",
+            "Easter Monday - April 1",
+            "Labour Day - May 1",
+            "Bastille Day - July 14",
+            "Assumption Day - August 15",
+            "All Saints' Day - November 1",
+            "Christmas Day - December 25"
+        ],
+        Philippines: [
+            "New Year's Day - January 1",
+            "Maundy Thursday - March 28",
+            "Good Friday - March 29",
+            "Araw ng Kagitingan - April 9",
+            "Labor Day - May 1",
+            "Independence Day - June 12",
+            "National Heroes Day - August 26",
+            "Bonifacio Day - November 30",
+            "Christmas Day - December 25",
+            "Rizal Day - December 30"
+        ],
+        'Saint Kitts and Nevis': [
+            "New Year's Day - January 1",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labor Day - May 1",
+            "Independence Day - September 19",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        Saint Lucia: [
+            "New Year's Day - January 1",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labor Day - May 1",
+            "Independence Day - February 22",
+            "Saint Lucia Day - December 13",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'Saint Vincent and the Grenadines': [
+            "New Year's Day - January 1",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labor Day - May 1",
+            "Independence Day - October 27",
+            "Bequia Day - January 21",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'Trinidad and Tobago': [
+            "New Year's Day - January 1",
+            "Carnival Monday - February 12",
+            "Carnival Tuesday - February 13",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labour Day - May 1",
+            "Independence Day - August 31",
+            "Republic Day - September 24",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'Turks and Caicos Islands': [
+            "New Year's Day - January 1",
+            "Good Friday - March 29",
+            "Easter Monday - April 1",
+            "Labour Day - May 1",
+            "Queen's Birthday - June 10",
+            "Constitution Day - October 5",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ],
+        'U.S. Virgin Islands': [
+            "New Year's Day - January 1",
+            "Three Kings Day - January 6",
+            "Martin Luther King Jr. Day - January 15",
+            "President's Day - February 19",
+            "Emancipation Day - July 3",
+            "Independence Day - July 4",
+            "Labor Day - September 2",
+            "Columbus Day - October 14",
+            "Christmas Day - December 25",
+            "Boxing Day - December 26"
+        ]
+    }
+};
+
+// Export the holidays object
+module.exports = holidays;
