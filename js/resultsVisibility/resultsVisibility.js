31f2de0fec38d2aba1c9318f8737246af8d4a4ab
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-22 16:17:29 +0800

    Add logging to `handleVisibilityChange` for debugging visibility conditions

--- js/resultsVisibility/resultsVisibility.js
+++ js/resultsVisibility/resultsVisibility.js
@@ -9,6 +9,10 @@ function handleVisibilityChange() {
     const serviceType = $('#serviceType').val();
     const country = $('#countrySelect').val();
 
+    // Log the current values of serviceType and country
+    console.log(`Service Type: ${serviceType}`);
+    console.log(`Country: ${country}`);
+
     // Always show #result but hide its text
     $('#result').removeClass('hidden');
     $('#result').val(''); // Hide text by setting it to empty
@@ -21,6 +25,9 @@ function handleVisibilityChange() {
 
     // Show additional fields if the conditions are met
     if (serviceType === 'standard' && country === 'United States') {
+        // Log that the conditions are met
+        console.log('Conditions met: Service Type is "standard" and Country is "United States".');
+
         $('#resultText1').text('Result for 3-6 Business Days'); // Set text for #resultText1
         $('#resultText1').removeClass('hidden'); // Show #resultText1
 
@@ -29,5 +36,10 @@ function handleVisibilityChange() {
 
         $('#standardResultField').removeClass('hidden');
         $('#standardResult').removeClass('hidden');
+    } else {
+        // Log that the conditions are not met and display the current selections
+        console.log('Conditions not met:');
+        console.log(`Current Service Type: ${serviceType}`);
+        console.log(`Current Country: ${country}`);
     }
 }
