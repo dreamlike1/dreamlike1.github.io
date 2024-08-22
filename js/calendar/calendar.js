6085232db6ad77394ecb8f2177160ad4ff41f381
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-22 10:34:25 +0800

    Update calendar.js

--- js/calendar/calendar.js
+++ js/calendar/calendar.js
@@ -20,24 +20,6 @@ export function initializeDateSelector(holidays = []) {
             message: holiday.name,
             class: 'holiday', // Use a CSS class for styling
             variation: 'holiday' // Tooltip variation (if supported)
-        })),
-        on: {
-            // Ensure that we highlight weekends after the calendar has been rendered
-            render: function() {
-                highlightWeekends();
-            }
-        }
-    });
-}
-
-function highlightWeekends() {
-    $('.ui.calendar .day').each(function() {
-        const $cell = $(this);
-        const date = new Date($cell.data('date')); // Adjust this if the data attribute is different
-        const dayOfWeek = date.getDay();
-
-        if (dayOfWeek === 0 || dayOfWeek === 6) { // 0 = Sunday, 6 = Saturday
-            $cell.addClass('weekend');
-        }
+        }))
     });
 }
