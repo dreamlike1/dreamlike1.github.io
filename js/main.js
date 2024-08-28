df7c9c6b0fb30b288782c862ae60c01e33b47efc
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-29 07:51:47 +0800

    Add Fomantic UI popups to result fields

--- js/main.js
+++ js/main.js
@@ -2,39 +2,42 @@ import { populateCountries } from './ui/countryUtils.js';
 import { setupEventListeners } from './eventHandlers/eventHandlers.js';
 import { setupSwitchButton } from './switch/switch.js';
 import { initializeTimezone } from './timezone/timezone.js';
-import { initializeDateSelector } from './calendar/calendar.js'; // Import the date selector module
-import { initializeResultsVisibility } from './resultsVisibility/resultsVisibility.js'; // Import the new module
+import { initializeDateSelector } from './calendar/calendar.js';
+import { initializeResultsVisibility } from './resultsVisibility/resultsVisibility.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const defaultServiceType = 'expressPaid'; // Default service type
+    const defaultServiceType = 'expressPaid';
 
-    // Function to format the date in a worded format
     function formatDateToWorded(date) {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Intl.DateTimeFormat('en-US', options).format(date);
     }
 
-    // Function to set today's date
     function setTodayDate() {
         const today = new Date();
         const formattedDate = formatDateToWorded(today);
         document.getElementById('couponDate').value = formattedDate;
     }
 
-    // Initialize Semantic UI dropdowns
+    // Initialize Semantic UI dropdowns and popups
     $('.ui.dropdown').dropdown();
-
-    // Populate countries dropdown and fetch holidays based on default serviceType
-    populateCountries(defaultServiceType).then(() => {
-        $('#countrySelect').dropdown('refresh'); // Ensure dropdown is refreshed
+    $('.result-field').popup({
+        on: 'hover',
+        variation: 'wide',
+        position: 'right center'
     });
 
-    setupEventListeners(); // Setup event listeners for UI elements
-    setupSwitchButton(); // Initialize the switch button functionality
-    initializeTimezone(); // Initialize the timezone functionality
-    initializeDateSelector(); // Initialize the date selector functionality
-    initializeResultsVisibility(); // Initialize results visibility logic
+    populateCountries(defaultServiceType).then(() => {
+        $('#countrySelect').dropdown('refresh');
+    }).catch(error => {
+        console.error('Error populating countries:', error);
+    });
 
-    // Set today's date as default value for couponDate input
+    setupEventListeners();
+    setupSwitchButton();
+    initializeTimezone();
+    initializeDateSelector();
+    initializeResultsVisibility();
+
     setTodayDate();
 });
